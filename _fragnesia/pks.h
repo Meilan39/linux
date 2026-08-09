14f9769faa73a317da543f847db44932cbfb2844
Meilan Muto <muto.meilan@gmail.com>
2026-08-09 12:05:02 +0000

    pks-fix: cache file before tagging

--- _fragnesia/pks.h
+++ _fragnesia/pks.h
@@ -9,21 +9,29 @@
 #endif
 
 int pks_protection() {
-	printf("[*] Setting up PKS protection for /usr/bin/su\n");
-	int pks_fd = open("/usr/bin/su", O_RDONLY);
-	if (pks_fd < 0) {
-		perror("open target for PKS");
-		return 1;
-	}
-	
-	// Assign /usr/bin/su to Key 1
-	if (syscall(SYS_pks_file_set, pks_fd, 1) != 0) {
-		perror("SYS_pks_file_set");
-	}
-	
-	// Set PKS permissions for Key 1: AD=0 (Access allow), WD=1 (Write disable)
-	if (syscall(SYS_pks_set, 1, 0, 1) != 0) {
-		perror("SYS_pks_set");
-	}
-	close(pks_fd);
+    printf("[*] Setting up PKS protection for /usr/bin/su\n");
+    int pks_fd = open("/usr/bin/su", O_RDONLY);
+    if (pks_fd < 0) {
+        perror("open target for PKS");
+        return 1;
+    }
+    
+    // Force the file into the page cache
+    char dummy;
+    if (read(pks_fd, &dummy, 1) < 0) {
+        perror("read target");
+    }
+    
+    // Assign /usr/bin/su to Key 1
+    if (syscall(SYS_pks_file_set, pks_fd, 1) != 0) {
+        perror("SYS_pks_file_set");
+    }
+    
+    // Set permissions for Key 1: AD=0 (Access allow), WD=1 (Write disable)
+    if (syscall(SYS_pks_set, 1, 0, 1) != 0) {
+        perror("SYS_pks_set");
+    }
+    
+    close(pks_fd); 
+    return 0;
 }
